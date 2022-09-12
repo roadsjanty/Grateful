ff44ad701b661bbff47ce23f5c0b44cbbdd215ae
ROADS <roadsjanty@gmail.com>
2022-09-13 05:17:46 +0530

    Initial commit

---
+++ app/src/main/java/com/peacon/grateful/models/MemoryGame.kt
@@ -0,0 +1,79 @@
+package com.peacon.grateful.models
+
+import com.peacon.grateful.utils.DEFAULT_ICONS
+
+class MemoryGame(
+    private val boardSize: BoardSize,
+    private val customImages: List<String>?
+    )  {
+
+    val cards: List<MemoryCard>
+    var numPairsFound = 0
+
+    private var numCardFlips=0
+    private var indexOfSingleSelectedCard : Int ? = null
+
+    init {
+        if (customImages == null){
+            val chosenImages = DEFAULT_ICONS .shuffled().take(boardSize.getNumPairs())
+            val randomizedImages = (chosenImages + chosenImages).shuffled()
+            cards = randomizedImages.map{MemoryCard(it)}
+        } else {
+            val randomizedImages = (customImages + customImages).shuffled()
+            cards = randomizedImages.map{MemoryCard( it.hashCode(),it)}
+        }
+    }
+
+    fun flipCard(position: Int):Boolean {
+        numCardFlips++
+        val card = cards[position]
+        // Three cases
+        // 0 cards previously flipped over => flip over the selected  card
+        // 1 card previously flipped over => flip over the selected card + check if the images match
+        // 2 card previously flipped over => restore cards + flip over the selected card
+        // you can never ever have three cards flipped over at once
+        var foundMatch = false
+        if (indexOfSingleSelectedCard == null) {
+            // 0 or 2 cards are previously flipper over
+            restoreCards()
+            indexOfSingleSelectedCard = position
+        } else {
+            // exactly 1 card is previously flipped over
+            // now acc to pseudo code flip over the card and check if image is matched
+            foundMatch = checkForMatch (indexOfSingleSelectedCard!!,position)
+            indexOfSingleSelectedCard = null
+        }
+        card.isFaceUp = !card.isFaceUp
+        return foundMatch
+    }
+
+    private fun checkForMatch(position1: Int, position2: Int): Boolean {
+       if (cards[position1].identifier != cards[position2].identifier){
+           return false
+       }
+        cards[position1].isMatched = true
+        cards[position2].isMatched = true
+        numPairsFound++
+        return true
+    }
+
+    private fun restoreCards() {
+        for (card in cards){
+            if (!card.isMatched){
+                card.isFaceUp = false
+            }
+        }
+    }
+
+    fun haveWonGame(): Boolean {
+        return numPairsFound == boardSize.getNumPairs()
+    }
+
+    fun isCardFaceUp(position: Int): Boolean {
+        return cards[position].isFaceUp
+    }
+
+    fun getNumMoves(): Int {
+        return numCardFlips / 2
+    }
+}
---
+++ app/src/main/java/com/peacon/grateful/models/userImageList.kt
@@ -0,0 +1,11 @@
+package com.peacon.grateful.models
+
+import com.google.firebase.firestore.PropertyName
+
+
+data class UserImageList (
+    @PropertyName("images") val images : List<String>? = null
+)
+
+
+
